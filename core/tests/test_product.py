f991f888bad3062442f960c14cc88a0cba0fb34d
WesleyVitor <wesleydemorais@outlook.com.br>
2022-11-26 14:24:43 -0300

    test: Criação de produtos com usuário sem permissão

--- core/tests/test_product.py
+++ core/tests/test_product.py
@@ -8,13 +8,22 @@ from core.models import Product
 EMAIL_USER="wesley@gmail.com"
 USERNAME_USER = "wesleyvitor"
 PASSWORD_USER = "123456aa"
+EMAIL_USER_WITHOUT_PERMISSION="wesley2@gmail.com"
+USERNAME_USER_WITHOUT_PERMISSION = "wesleyvitor2"
+PASSWORD_USER_WITHOUT_PERMISSION = "123456aa"
 
 class ProductTest(TestCase):
     @classmethod
     def setUpTestData(self):
         
-        self.user:User = User.objects.create_user(email=EMAIL_USER,username=USERNAME_USER,password=PASSWORD_USER,is_staff=True)
+        self.user:User = User.objects.create_user(email=EMAIL_USER,username=USERNAME_USER,
+        password=PASSWORD_USER,is_staff=True)
         self.user.save()
+        self.user_without_permission:User = User.objects.create_user(email=EMAIL_USER_WITHOUT_PERMISSION,
+        username=USERNAME_USER_WITHOUT_PERMISSION,
+        password=PASSWORD_USER_WITHOUT_PERMISSION,
+        is_staff=False)
+        self.user_without_permission.save()
     
     
     def test_product_list_after_authenticate(self):
@@ -38,3 +47,12 @@ class ProductTest(TestCase):
         self.assertEqual(product.name,'arroz')
         self.assertEqual(product.description,'muito bom')
         self.assertEqual(product.price,20)
+    def test_product_created_without_permission(self):
+        
+        cliente = Client()
+        cliente.login(username=USERNAME_USER_WITHOUT_PERMISSION,password=PASSWORD_USER_WITHOUT_PERMISSION)
+        url = reverse('product')
+        response = cliente.post(url, {"name":"arroz", "description":"muito bom", "price":"20"})
+        
+        self.assertEqual(response.status_code, 403)
+        
