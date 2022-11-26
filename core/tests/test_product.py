001800b996dfd0b5f94731af5c4e1db0d2e294df
WesleyVitor <wesleydemorais@outlook.com.br>
2022-11-26 14:19:30 -0300

    test: Criação de produtos usando permissão de is_staff do usuário

--- core/tests/test_product.py
+++ core/tests/test_product.py
@@ -2,7 +2,7 @@ from django.test import TestCase, Client
 from django.urls import reverse
 
 from django.contrib.auth.models import User
-
+from core.models import Product
 
 
 EMAIL_USER="wesley@gmail.com"
@@ -26,3 +26,15 @@ class ProductTest(TestCase):
         response = cliente.get(url)
         
         self.assertEqual(response.status_code, 200)
+    def test_product_created_after_authenticate(self):
+        
+        cliente = Client()
+        cliente.login(username=USERNAME_USER,password=PASSWORD_USER)
+        
+        url = reverse('product')
+        response = cliente.post(url, {"name":"arroz", "description":"muito bom", "price":"20"})
+        product = Product.objects.get(id=1)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(product.name,'arroz')
+        self.assertEqual(product.description,'muito bom')
+        self.assertEqual(product.price,20)
