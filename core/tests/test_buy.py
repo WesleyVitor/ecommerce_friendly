d29916cd8d69c7bee7e85c3ea1d8d2108dd7a9f0
WesleyVitor <wesleydemorais@outlook.com.br>
2022-12-01 10:38:20 -0300

    test: Fazer a compra apartir do carrinho de compras feita pelo usuário logado

--- core/tests/test_buy.py
+++ core/tests/test_buy.py
@@ -1,14 +1,15 @@
 from django.test import TestCase, client
-from core.models import Product, ShoppingCart, ItemProduct, Buy
 from django.contrib.auth.models import User
 from django.urls import reverse
+from core.models import ShoppingCart, Product, Buy
 EMAIL_USER="wesley@gmail.com"
 USERNAME_USER = "wesleyvitor"
 PASSWORD_USER = "123456aa"
 EMAIL_USER_WITHOUT_PERMISSION="wesley2@gmail.com"
 USERNAME_USER_WITHOUT_PERMISSION = "wesleyvitor2"
 PASSWORD_USER_WITHOUT_PERMISSION = "123456aa"
-class ShoppingCartTest(TestCase):
+class BuyTest(TestCase):
+    
     @classmethod
     def setUpTestData(self):
         
@@ -20,7 +21,8 @@ class ShoppingCartTest(TestCase):
         password=PASSWORD_USER_WITHOUT_PERMISSION,
         is_staff=False)
         self.user_without_permission.save()
-    def test_route_shoppingcart_view(self):
+
+    def test_route_buy_view(self):
         cliente = client.Client()
         cliente.login(username=USERNAME_USER, password=PASSWORD_USER)
 
@@ -30,70 +32,37 @@ class ShoppingCartTest(TestCase):
             "product":p1.pk,
             "amount":30 
         }
-        response = cliente.post(url,data)
-        self.assertEqual(response.status_code, 201)
-    
-    def test_creating_a_shoppingcart(self):
-        cliente = client.Client()
-        cliente.login(username=USERNAME_USER, password=PASSWORD_USER)
-
-        p1 = Product.objects.create(name='Arroz', description="Muito Bom", price=20)
+        cliente.post(url,data)
         
 
-        data = {
-            "product":p1.pk,
-            "amount":30 
-        }
-        url = reverse("shoppingcart")
-        response = cliente.post(url, data)
+        url = reverse("buy")
         
-
-        self.assertEqual(ItemProduct.objects.count(), 1)
-        self.assertEqual(ShoppingCart.objects.count(), 1)
-
-    def test_without_pass_payload(self):
-        cliente = client.Client()
-        cliente.login(username=USERNAME_USER, password=PASSWORD_USER)
-
-        
-        url = reverse("shoppingcart")
         response = cliente.post(url)
-        self.assertEqual(response.status_code, 406)
-        
-    def test_product_not_exist(self):
-        cliente = client.Client()
-        cliente.login(username=USERNAME_USER, password=PASSWORD_USER)
-
-        data = {
-            "product":1,
-            "amount":30 
-        }
-        
-        url = reverse("shoppingcart")
-        response = cliente.post(url,data)
-        self.assertEqual(response.status_code, 400)
-
-    def test_add_two_product_to_some_shoppingcart(self):
+        self.assertEqual(response.status_code, 201)
+    def test_do_a_buy(self):
         cliente = client.Client()
         cliente.login(username=USERNAME_USER, password=PASSWORD_USER)
 
         p1 = Product.objects.create(name='Arroz', description="Muito Bom", price=20)
+        url = reverse("shoppingcart")
         data = {
             "product":p1.pk,
             "amount":30 
         }
-        url = reverse("shoppingcart")
-        cliente.post(url, data)
+        cliente.post(url,data)
+        sc = ShoppingCart.objects.get(id=1)
+
+        url = reverse("buy")
         
-        p2 = Product.objects.create(name='Batata', description="Muito Bom", price=10)
-        data = {
-            "product":p2.pk,
-            "amount":30 
-        }
-        url = reverse("shoppingcart")
-        cliente.post(url, data)
+        response = cliente.post(url)
+        self.assertEqual(Buy.objects.count(),1)
+    def test_without_shoppingcart_to_buy(self):
+        cliente = client.Client()
+        cliente.login(username=USERNAME_USER, password=PASSWORD_USER)
+
+        url = reverse("buy")
         
-        list_items = ShoppingCart.objects.get(id=1).list_items.all()
-        a=1
-        self.assertEqual(list_items.count(), 2)
-        
+        response = cliente.post(url)
+        self.assertEqual(response.status_code, 400)
+    
+   
